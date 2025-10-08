f001047e6f895b3f5810c36a2bce08ae0f22ce70
DavelDs <163164280+DavelDs@users.noreply.github.com>
2025-10-08 13:28:36 +0700

    Commit No. 3 – Move Google Maps API Key to secure location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,45 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+}
 
+android {
+    namespace = "com.example.lab_week_07"
+    compileSdk = 36
 
-    plugins {
-        alias(libs.plugins.android.application)
-        alias(libs.plugins.kotlin.android)
-        alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+    defaultConfig {
+        applicationId = "com.example.lab_week_07"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    android {
-        namespace = "com.example.lab_week_07"
-        compileSdk = 36
-
-        defaultConfig {
-            applicationId = "com.example.lab_week_07"
-            minSdk = 24
-            targetSdk = 36
-            versionCode = 1
-            versionName = "1.0"
-
-            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        }
-
-        buildTypes {
-            release {
-                isMinifyEnabled = false
-                proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
-                )
-            }
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
-        }
-        kotlinOptions {
-            jvmTarget = "11"
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+secrets {
+    propertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "local.defaults.properties"
+}
 
     dependencies {
         implementation(libs.play.services.maps)
